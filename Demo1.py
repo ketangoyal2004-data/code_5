f00dde5b0e5014b7b76da0e0e6a32384d8cc9bb5
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-17 11:50:19 +0530

    This a new files

--- Demo1.py
+++ Demo1.py
@@ -1,8 +1,10 @@
 class std:
-    def __init__(self,name,course,branch):
+    def __init__(self,name,course,branch,city):
         self.branch = branch
         self.course = course
         self.name = name
+        self.city = city
         print(f"Name: {self.name} Course: {self.course} Branch: {self.branch}")
 
-obj = std("John","B.Tech","CSE")
+
+obj = std("John","B.Tech","CSE","Jaipur")
--- Demo2.py
+++ Demo2.py
@@ -1,10 +1,12 @@
 class cal:
-    def __init__(self,a,b):
+    def __init__(self,a,b,c):
             self.a = a
             self.b = b
+            self.c = c
             print("a + b = ",self.a + self.b)
             print("a - b = ",self.a - self.b)
             print("a * b = ",self.a * self.b)
-            print("a / b = ",self.a / self.b)   
+            print("a / b = ",self.a / self.b)
+            print("a * b * c = ",self.a * self.b * self.c)   
 
-obj = cal(10,5)
+obj = cal(10,5,2)
