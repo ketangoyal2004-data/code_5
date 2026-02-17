22bb1beb93e9b75f177d5f751bd712404ecb8809
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-17 11:55:36 +0530

    This is a new files

---
+++ Demo3.py
@@ -0,0 +1,14 @@
+class cal:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        print("a + b = ",self.a + self.b)
+
+    def sub(self):
+        print("a - b = ",self.a - self.b)
+
+obj = cal(10,5)
+obj.add()
+obj.sub()
